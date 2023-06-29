c4d5598df29716a0f363dcdcc07739f5ae127e04
sara-rioseco <sara.rioseco@gmail.com>
2023-06-28 22:23:10 -0400

    adds mdLinks fx

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-module.exports = () => {
-  // ...
+export const mdLinks = (path, options) => {
+  const linksArr = [];
+  const links = findLinksInMdFile(path);
+  const promiseWithValidate = () => new Promise((resolve, reject)
+    .then(links.forEach(link => { 
+      const linkObj = new Object;
+      linkObj.href = '';
+      linkObj.text = '';
+      linkObj.file = `${path}`;
+      linkObj.status = '';
+      linkObj.ok = '';
+      linksArr.push(linkObj);
+      return linksArr;
+    }))
+    .catch(err => err));
+  const promiseWithoutValidate = () => new Promise((resolve, reject)
+    .then(links.forEach(link => { 
+      const linkObj = new Object;
+      linkObj.href = '';
+      linkObj.text = '';
+      linkObj.file = '';
+      linksArr.push(linkObj);
+      return linksArr;
+    }))
+    .catch(err => err));
+  options = {validate : true} ? promiseWithValidate() : promiseWithoutValidate();
+};
+
+export const findLinksInMdFile = path => {
+
 };
