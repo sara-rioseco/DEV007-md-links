ffa40d208fe0d99c0aa7fbb3e1087247600b8f20
sara-rioseco <sara.rioseco@gmail.com>
2023-07-04 14:18:44 -0400

    adds more regex

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // importing modules
 import fs from 'fs';
 import path from 'path';
+import { resolve } from 'path';
 import argv from 'process';
 import env from 'process';
 import process from 'process';
@@ -8,7 +9,7 @@ import { Renderer, marked } from 'marked';
 import DOMPurify from 'isomorphic-dompurify';
 import { resourceLimits } from 'worker_threads';
 
-// getting an array with all MD files in a directory
+// getting an array with all MD files in a specific directory
 export const getMdFilesArr = dir => {
   const files = fs.readdirSync(dir, )
   const mdFilesArr = []
@@ -36,16 +37,25 @@ export const getLinks = (str) => {
 
 // separating the text from the href, returning an Obj with both as properties
 export const separateLinks = (str) => {
-  const separatedRegex = /^\[([\w\s\d]+)\]\(((?:\/|https?:\/\/)[\w\d./?=#]+)\)$/
-  const results = str.match(separatedRegex);
-  return results;
+  const textRegex = /!?\[([^\]]*)\]\(/gm
+  const hrefRegex = /\(([^\)]+)\)/gm
+  const text = str.match(textRegex);
+  const href = str.match(hrefRegex);
+  const linkObj = new Object;
+  linkObj.text = text.toString().slice(1, -2);
+  linkObj.href = href.toString().slice(1, -1);
+  return linkObj;
 };
 
 // getting links in a specific MD file
-export const findLinksInMdFile = path => { };
+export const findLinksInMdFile = path => {
 
-//getting links in All MD Files in a specific directory
-export const findLinksInAllMdFilesInDir = dir => { };
+};
+
+// getting links in All MD Files in a specific directory
+export const findLinksInAllMdFilesInDir = dir => {
+
+};
 
 // getting the links from a specific path with specific options
 export const mdLinks = (path, options) => {
@@ -82,17 +92,19 @@ const mdFilesHere = getMdFilesArr('./'); // MD files in this path
 const mdFileContent = readMdFile(mdFilesHere[0]); // Content of the specific MD file
 const linksInThisMdFile = getLinks(mdFileContent); // Links in this MD File text and href together
 const linksArr = Object.values(linksInThisMdFile); // Array of links as objects with both text and href properties
+const linkObjSeparated = separateLinks(linksArr[0]);
 
-console.log(linksArr[0])
-console.log(linksArr[1])
-console.log(linksArr[2])
-console.log(linksArr[3])
-console.log(linksArr[4])
-console.log(linksArr[5])
+console.log(linksArr[0]);
+console.log(linksArr[1]);
+console.log(linksArr[2]);
+console.log(linksArr[3]);
+console.log(linksArr[4]);
+console.log(linksArr[5]);
+console.log(linkObjSeparated);
 
 
 /* ======================= MARKED =======================
-//sanitizing html
+// sanitizing html
 const sanitizedHTML = DOMPurify.sanitize(getLinks(mdFileContent));
 
 // parsing MD contents to HTML
